d59626758b7c4de79c256af1b4361d4edde1e958
unknown <theonlyrt6@gmail.com>
2017-06-11 16:50:27 +0800

    added simple login and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 TABAO README
+
+Directory Description:
+-client: all client-side stuff (html/css/js)
+-imports: external references (currently only for account config)
+-lib: libraries for fundamental app logic (currently only for flow-router)
+-server: driver program
--- client/main.js
+++ client/main.js
@@ -1,22 +1,2 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-
+import '../imports/startup/accounts-config.js';
 import './app.html';
-
-Template.main.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.main.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.main.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
