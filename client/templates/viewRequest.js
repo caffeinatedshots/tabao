e4ab25fa7d8e5e5e123ae4e102a989f59eeb1077
unknown <theonlyrt6@gmail.com>
2017-07-24 21:56:58 +0800

    fixed map opening in same tab and added date to viewRequest

--- client/templates/viewRequest.js
+++ client/templates/viewRequest.js
@@ -2,12 +2,14 @@ import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { Session } from 'meteor/session';
 import { Requests } from '../../imports/requestsDB.js';
+import { Hawkers } from '../../imports/hawkersDB.js';
 
 import './viewRequest.html';
 
 Template.viewRequest.onCreated(function() {
 	var requestId = FlowRouter.getParam("requestId");
 	Meteor.subscribe("itemRequest", requestId);
+	Meteor.subscribe("allHawkers");
 });
 
 Template.viewRequest.events({
@@ -64,10 +66,7 @@ Template.viewRequest.helpers({
 	},
 
 	getHawkerName(postal){
-		Meteor.call('getHawkerName', parseInt(postal), function(error, result){
-			Session.set('hawkerName', result);
-		});
-		return Session.get('hawkerName');
+		return Hawkers.findOne({_id:parseInt(postal)}).Name;
 	},
 
 	mapLink(fromLocation, toLocation){
